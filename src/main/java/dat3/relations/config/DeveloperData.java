ae18a29058325dfe460e3a649d5e0ead81e49cf7
plaul <lmor@kea.dk>
2023-02-13 10:49:45 +0100

    step5

--- src/main/java/dat3/relations/config/DeveloperData.java
+++ src/main/java/dat3/relations/config/DeveloperData.java
@@ -1,7 +1,8 @@
 package dat3.relations.config;
 
-import dat3.relations.entity.Address;
-import dat3.relations.entity.Citizen;
+import dat3.relations.repositories.AddressRepository;
+import dat3.relations.repositories.CitizenRepository;
+import dat3.relations.repositories.TownRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class DeveloperData implements ApplicationRunner {
 
+  TownRepository townRepository;
+  AddressRepository addressRepository;
+  CitizenRepository citizenRepository;
+
+  public DeveloperData(TownRepository townRepository, AddressRepository addressRepository, CitizenRepository citizenRepository) {
+    this.townRepository = townRepository;
+    this.addressRepository = addressRepository;
+    this.citizenRepository = citizenRepository;
+  }
 
   @Override
   public void run(ApplicationArguments args) throws Exception {
     System.out.println("Called");
   }
-
 
 }
